46b602c5119e4d66cba2b9c01c7317e1cd3cea01
Etienne Carriere <etienne.a.carriere@socgen.com>
2021-11-03 06:43:06 +0100

    Transform internal load balancer to external one

--- modules/load_balancer/main.tf
+++ modules/load_balancer/main.tf
@@ -1,15 +1,13 @@
-resource "google_compute_address" "internal" {
-  name         = "${var.resource_name_prefix}-vault-internal-lb"
-  subnetwork   = var.subnetwork
-  address_type = "INTERNAL"
-  purpose      = "GCE_ENDPOINT"
+resource "google_compute_address" "external" {
+  name         = "${var.resource_name_prefix}-vault-lb"
+  address_type = "EXTERNAL"
 }
 
 resource "google_compute_region_health_check" "lb" {
-  name = "${var.resource_name_prefix}-vault-interal-lb"
+  name = "${var.resource_name_prefix}-vault-lb"
 
   check_interval_sec = 30
-  description        = "The health check of the internal load balancer for Vault"
+  description        = "The health check of the load balancer for Vault"
   timeout_sec        = 4
 
   https_health_check {
@@ -20,9 +18,9 @@ resource "google_compute_region_health_check" "lb" {
 
 resource "google_compute_region_backend_service" "lb" {
   health_checks = [google_compute_region_health_check.lb.self_link]
-  name          = "${var.resource_name_prefix}-vault-internal-lb"
+  name          = "${var.resource_name_prefix}-vault-lb"
 
-  description           = "The backend service of the internal load balancer for Vault"
+  description           = "The backend service of the load balancer for Vault"
   load_balancing_scheme = "INTERNAL_MANAGED"
   port_name             = "https"
   protocol              = "HTTPS"
@@ -39,13 +37,13 @@ resource "google_compute_region_backend_service" "lb" {
 
 resource "google_compute_region_url_map" "lb" {
   default_service = google_compute_region_backend_service.lb.self_link
-  name            = "${var.resource_name_prefix}-vault-internal-lb"
+  name            = "${var.resource_name_prefix}-vault-lb"
 
   description = "The URL map of the internal load balancer for Vault"
 }
 
 resource "google_compute_region_target_https_proxy" "lb" {
-  name             = "${var.resource_name_prefix}-vault-internal-lb"
+  name             = "${var.resource_name_prefix}-vault-lb"
   ssl_certificates = [var.ssl_certificate_name]
   url_map          = google_compute_region_url_map.lb.self_link
 
@@ -53,11 +51,10 @@ resource "google_compute_region_target_https_proxy" "lb" {
 }
 
 resource "google_compute_forwarding_rule" "lb" {
-  name                  = "${var.resource_name_prefix}-vault-internal-lb"
-  ip_address            = google_compute_address.internal.address
+  name                  = "${var.resource_name_prefix}-vault-lb"
+  ip_address            = google_compute_address.external.address
   ip_protocol           = "TCP"
-  load_balancing_scheme = "INTERNAL_MANAGED"
+  load_balancing_scheme = "EXTERNAL"
   port_range            = 443
-  subnetwork            = var.subnetwork
   target                = google_compute_region_target_https_proxy.lb.self_link
 }
